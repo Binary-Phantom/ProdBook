9782a3f4103f09faabb100ddf5026d83de8c7d91
Binary-Phantom <profissional.viniciusproenca@gmail.com>
2023-04-02 23:10:21 -0300

    v1.5.1

--- views/site/about.php
+++ views/site/about.php
@@ -11,7 +11,18 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        AMOEBA
+    Produtor Book é uma rede social criada especialmente para produtores rurais de todo o mundo. A plataforma tem como objetivo conectar agricultores, pecuaristas e outros profissionais do campo, permitindo que eles troquem informações e conhecimentos sobre técnicas de produção, inovações tecnológicas, gestão financeira e outros assuntos relacionados à agricultura.
+
+A plataforma oferece uma ampla variedade de recursos, como fóruns de discussão, grupos de interesse, ferramentas de gestão agrícola, informações sobre mercado e preços, além de uma área exclusiva para compra e venda de produtos agropecuários.
+
+Por meio da Produtor Book, os produtores rurais podem compartilhar suas experiências e aprender com outros colegas da área, além de se conectar com fornecedores de insumos, compradores de produtos agrícolas, empresas de processamento e distribuição de alimentos e outros parceiros do setor.
+
+A rede social é uma ferramenta poderosa para ajudar a fortalecer as comunidades agrícolas em todo o mundo, promovendo a colaboração, a inovação e a sustentabilidade no setor agrícola. Com a Produtor Book, os produtores rurais têm acesso a informações importantes e podem tomar decisões mais informadas em suas atividades diárias, além de terem a oportunidade de expandir seus negócios e fazer novas conexões profissionais.
+
+
+
+
+
     </p>
 
     <code><?= __FILE__ ?></code>
